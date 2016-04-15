4d0ee4a9c0c13c88d89a682c2aefe144316c91a8
Bryan Kroesbeek <BryanK.B96@gmail.com>
2016-04-16 01:11:19 +0200

    created an interface for the smasher

---
+++ KeyboardSmasher/KeyboardSmasher/ISmasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardSmasher {
+    interface ISmasher {
+        bool IsSmashing();
+        void StartSmashing();
+        void StopSmashing();
+        void Smash();
+    }
+}
